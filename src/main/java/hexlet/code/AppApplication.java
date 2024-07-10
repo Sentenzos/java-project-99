a989dd8fc9749077843eb66f989dd472cf33fe47
sentenzos <sentenzoss@gmail.com>
2024-07-10 22:12:01 +0300

    sentry

--- src/main/java/hexlet/code/AppApplication.java
+++ src/main/java/hexlet/code/AppApplication.java
@@ -1,5 +1,6 @@
 package hexlet.code;
 
+import io.sentry.spring.jakarta.EnableSentry;
 import net.datafaker.Faker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,6 +9,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableSentry(dsn = "https://83f49941acca138380ae24b8c94185a6@o4507578897530880.ingest.de.sentry.io/4507578905854032")
 public class AppApplication {
     public static void main(String[] args) {
         SpringApplication.run(AppApplication.class, args);
